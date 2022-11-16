05e144c57771baf97dad044b5ef5702f0ca43bd2
Kevin Trinh <kevin@Kevins-iMac.local>
2022-11-16 09:29:53 -0600

    create react app

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <a href="/">Kevin's Reef Store</a>
+      </header>
+       <main>
+        list product
+      </main>
+
+
+    </div>
+  );
+}
+
+export default App;
