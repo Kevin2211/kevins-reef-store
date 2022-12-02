19bd9a0f0265a83d2e79ecbde1103e164f6875ef
Kevin Trinh <trinh_thanh42@yahoo.com>
2022-12-02 08:51:56 -0600

    added signin and signout function, added toastify alert message

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,20 +3,29 @@ import './App.css';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
-import {Navbar, Container, Nav, Badge} from 'react-bootstrap';
+import {Navbar, Container, Nav, Badge, NavDropdown} from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap'
 import { useContext } from 'react';
 import { Store } from './Store';
 import CartScreen from './screens/CartScreen';
 import SigninScreen from './screens/SigninScreen';
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 function App() {
-  const { state } = useContext(Store)
-  const { cart } = state
+  const { state, dispatch: contextDispatch } = useContext(Store)
+  const { cart, userInfo } = state
+
+  const signoutHandler = () => {
+    contextDispatch({ type: 'USER-SIGNOUT' })
+    localStorage.removeItem('userInfo')
+    console.log(state.userInfo)
+  }
   return (
 
     <BrowserRouter>
       <div className='d-flex flex-column site-container'>
+      <ToastContainer position='bottom-center' limit={1}/>
       <header>
         <Navbar bg="primary" variant="dark" expand="lg">
           <Container>
@@ -33,6 +42,18 @@ function App() {
                 )
                 }
               </Link>
+              {userInfo ? (
+                <NavDropdown title={userInfo.name} id="nav-dropdown">
+                    <LinkContainer to='/profile'>
+                      <NavDropdown.Item> User Profile</NavDropdown.Item>
+                    </LinkContainer>
+                    <LinkContainer to='/orderhistory'>
+                      <NavDropdown.Item> Orders</NavDropdown.Item>
+                    </LinkContainer>
+                    <NavDropdown.Divider></NavDropdown.Divider>
+                    <Link to='#signout' className='dropdown-item' onClick={signoutHandler}>Sign Out</Link>
+                </NavDropdown>
+              ):( <Link className="nav-link" to='/signin'> Sign In</Link> )}
             </Nav>
           </Container>
         </Navbar>
--- frontend/src/screens/SigninScreen.js
+++ frontend/src/screens/SigninScreen.js
@@ -1,13 +1,45 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect } from 'react'
+import { useContext } from 'react'
+import { useState } from 'react'
 import { Button, Container, Form } from 'react-bootstrap'
 import { Helmet } from 'react-helmet-async'
-import { useLocation } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
 import { Link } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import { Store } from '../Store'
+import { getError } from '../utils'
 
 export default function SigninScreen() {
+    const navigate = useNavigate()
     const { search } = useLocation()
     const redirectInUrl = new URLSearchParams(search).get('redirect')
     const redirect = redirectInUrl ? redirectInUrl : '/'
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const { state, dispatch: contextDispatch } = useContext(Store)
+    const userInfo = state.userInfo
+    const submitHandler = async (e) => {
+        e.preventDefault()
+        try {
+            const { data } = await axios.post('/api/users/signin', {
+                email,
+                password
+            })
+            contextDispatch({type: 'USER_SIGNIN', payload: data})
+            localStorage.setItem('userInfo', JSON.stringify(data))
+            navigate(redirect || '/')
+        } catch (error) {
+            toast.error(getError(error))
+        }
+    }
+    useEffect(() => {
+        if(userInfo){
+            navigate(redirect)
+        }
+    }, [navigate, redirect, userInfo])
 
   return (
     <Container className="small-container">
@@ -15,14 +47,14 @@ export default function SigninScreen() {
             <title>Sign In</title>
         </Helmet>
         <h1 className='my-3'>Sign In</h1>
-        <Form>
+        <Form onSubmit={ submitHandler }>
             <Form.Group className='mb-3' controlId='email'>
                 <Form.Label>Email</Form.Label>
-                <Form.Control type='email' required></Form.Control>
+                <Form.Control type='email' onChange={(e) => setEmail(e.target.value)} required></Form.Control>
             </Form.Group>
             <Form.Group className='mb-3' controlId='password'>
                 <Form.Label>password</Form.Label>
-                <Form.Control type='password' required></Form.Control>
+                <Form.Control type='password'  onChange={(e) => setPassword(e.target.value)} required></Form.Control>
             </Form.Group>
             <div className='mb-3'>
                 <Button type='submit'>Sign In</Button>
